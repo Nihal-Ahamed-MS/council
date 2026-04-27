4c2e8e18e7fee7455435f7ee164e78737d3ba80e
Nihal-Ahamed-MS <nihaljesiya@gmail.com>
2026-04-27 22:16:15 +0530

    fixed shebang

--- build.mjs
+++ build.mjs
@@ -3,6 +3,20 @@ import { mkdir } from 'fs/promises';
 
 await mkdir('dist', { recursive: true });
 
+const stubPlugin = {
+  name: 'stub-missing',
+  setup(build) {
+    build.onResolve({ filter: /^react-devtools-core$/ }, () => ({
+      path: 'react-devtools-core',
+      namespace: 'stub',
+    }));
+    build.onLoad({ filter: /.*/, namespace: 'stub' }, () => ({
+      contents: 'export default {}',
+      loader: 'js',
+    }));
+  },
+};
+
 await esbuild.build({
   entryPoints: ['bin/council.js'],
   bundle: true,
@@ -12,6 +26,7 @@ await esbuild.build({
   outfile: 'dist/council.js',
   jsx: 'transform',
   external: ['fsevents'],
+  plugins: [stubPlugin],
 });
 
 console.log('✓ dist/council.js');
